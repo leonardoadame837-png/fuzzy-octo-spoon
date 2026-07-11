f0de5b1c81e6c3d9b21f9ea6ae98c4c262374351
leonardoadame837-png <leonardoadame837@gmail.com>
2026-07-11 20:45:27 +0800

    chore: cache SitePilot app icon offline

--- docs/sitepilot-ai/app/service-worker.js
+++ docs/sitepilot-ai/app/service-worker.js
@@ -1,7 +1,14 @@
 "use strict";
 
-const CACHE_NAME = "sitepilot-shell-v1";
-const APP_SHELL = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest"];
+const CACHE_NAME = "sitepilot-shell-v2";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon.svg"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
